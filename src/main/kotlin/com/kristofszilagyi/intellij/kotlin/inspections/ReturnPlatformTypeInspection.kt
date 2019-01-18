17a9a95b940f07e9ea95d4ab2d34cb3ccd4b3c2a
kszilagyi <kristof.szilagyi1@gmail.com>
2019-01-18 16:21:28 +0000

    Som expression function syntax works now

--- src/main/kotlin/com/kristofszilagyi/intellij/kotlin/inspections/ReturnPlatformTypeInspection.kt
+++ src/main/kotlin/com/kristofszilagyi/intellij/kotlin/inspections/ReturnPlatformTypeInspection.kt
@@ -7,9 +7,7 @@ import com.intellij.psi.util.parentOfType
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.debugger.sequence.psi.resolveType
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
-import org.jetbrains.kotlin.psi.KtCallableDeclaration
-import org.jetbrains.kotlin.psi.KtReturnExpression
-import org.jetbrains.kotlin.psi.KtVisitorVoid
+import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.kotlin.types.isFlexible
@@ -21,24 +19,41 @@ class ReturnPlatformTypeInspection : AbstractKotlinInspection() {
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : KtVisitorVoid() {
+            override fun visitNamedFunction(function: KtNamedFunction) {
+                super.visitNamedFunction(function)
+                function.bodyExpression?.let { body ->
+                    val context = function.analyze(BodyResolveMode.PARTIAL)
+                    val functionReturnType = context.get(BindingContext.TYPE, function.typeReference)
+                    val bodyReturnType = body.resolveType()
+                    if (functionReturnType != null) {
+                        if(bodyReturnType.unwrap().isFlexible() && !functionReturnType.isFlexible() && !functionReturnType.isMarkedNullable) {
+                            registerProblem(holder, body)
+                        }
+                    }
+                }
+            }
 
             override fun visitReturnExpression(expression: KtReturnExpression) {
                 super.visitReturnExpression(expression)
                 val typeInReturn = expression.returnedExpression?.resolveType()
-                val context = expression.analyze(BodyResolveMode.PARTIAL);
+                val context = expression.analyze(BodyResolveMode.PARTIAL)
                 val functionReturnType = context.
                     get(BindingContext.TYPE, expression.parentOfType(KtCallableDeclaration::class)?.typeReference)
 
                 if (typeInReturn != null && functionReturnType != null) {
                     if (typeInReturn.unwrap().isFlexible() && !functionReturnType.isFlexible() && !functionReturnType.isMarkedNullable) {
-                        holder.registerProblem(expression,
-                            "You are implicitly converting a platform type into a non-nullable type. This code might throw.",
-                            ProblemHighlightType.GENERIC_ERROR_OR_WARNING)
+                        registerProblem(holder, expression)
                     }
                 }
             }
+        }
+    }
 
-
+    companion object {
+        fun registerProblem(holder: ProblemsHolder, expression: KtExpression) {
+            holder.registerProblem(expression,
+                "You are implicitly converting a platform type into a non-nullable type. This code might throw.",
+                ProblemHighlightType.GENERIC_ERROR_OR_WARNING)
         }
     }
 }
--- testData/ImplicitPlatformTypeConversion/ConvertPlatformTypeOnReturn.kt
+++ testData/ImplicitPlatformTypeConversion/ConvertPlatformTypeOnReturn.kt
@@ -2,6 +2,24 @@
 
 class ConvertPlatformTypeOnReturn {
     fun getValue(): Value {
-        <error descr="You are implicitly converting a platform type into a non-nullable type. This code might throw.">return MyClass.value()</error>;
+        <error descr="You are implicitly converting a platform type into a non-nullable type. This code might throw.">return MyClass.value()</error>
     }
+
+    fun getValueExpression(): Value =
+        <error descr="You are implicitly converting a platform type into a non-nullable type. This code might throw.">MyClass.value()</error>
+
+    fun getValueNullable(): Value? {
+        return MyClass.value()
+    }
+
+    fun getValueNullableExpression(): Value? = MyClass.value()
+
+    fun getValuePlatformExpression() = MyClass.value()
+
+    fun getValueExpressionComplex(): Value = if (1.hashCode() > 2) {
+        MyClass.value()
+    } else {
+        Value()
+    }
+
 }
