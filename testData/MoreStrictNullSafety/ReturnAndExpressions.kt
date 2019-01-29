83a75b35527264542300b11498b6c61ece0d5c2c
kszilagyi <kristof.szilagyi1@gmail.com>
2019-01-29 21:09:50 +0000

    More tests

--- testData/MoreStrictNullSafety/ReturnAndExpressions.kt
+++ testData/MoreStrictNullSafety/ReturnAndExpressions.kt
@@ -221,6 +221,21 @@ class ReturnAndExpressions {
     fun functionCallNormalNullable(): Unit {
         functionNullable(Value())
     }
+
+    fun lambdaCall(): Unit {
+        val lambda = {v: Value -> v}
+        lambda(<error descr="You are implicitly converting a platform type into a non-nullable type. This code might throw.">JavaClass.value()</error>)
+    }
+
+    fun lambdaCallNormal(): Unit {
+        val lambda = {v: Value -> v}
+        lambda(Value())
+    }
+
+    fun lambdaCallNullable(): Unit {
+        val lambda = {v: Value? -> v}
+        lambda(JavaClass.value())
+    }
 }
 
 //fun parseAndInc(number: String?): Int {
