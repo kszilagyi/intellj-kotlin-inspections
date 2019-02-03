df99b2575bf8e6ff15dbdc89134d948d8216eaf6
kszilagyi <kristof.szilagyi1@gmail.com>
2019-02-03 08:41:22 +0000

    Added assignment tests

--- testData/MoreStrictNullSafety/tests/Mixed.kt
+++ testData/MoreStrictNullSafety/tests/Mixed.kt
@@ -1,4 +1,25 @@
-//todo
+
+
+fun assinment() {
+    val value: Value = <error descr="Implicit conversion of platform type to non-nullable">JavaClass.value()</error>
+    value.toString()
+}
+
+fun assinmentNullable() {
+    val value: Value? = JavaClass.value()
+    value.toString()
+}
+
+fun assinmentNonPlatform() {
+    val value: Value = Value()
+    value.toString()
+}
+
+fun assinmentInferred() {
+    val value = JavaClass.value()
+    val value2: Value = <error descr="Implicit conversion of platform type to non-nullable">value</error>
+    value2.toString()
+}
 
 //todo add assignment tests
 //todo add overrides
